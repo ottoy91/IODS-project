b371106a9ddfff1e308641360cfd87591c96c1fa
ottoy91 <otto.ylostalo@helsinki.fi>
2018-11-25 21:09:39 +0200

    Update create_human.R

--- create_human.R
+++ create_human.R
@@ -1,3 +1,5 @@
+### 25.11.2018/Otto Ylöstalo/IODS-project Week 4 ###
+
 #1
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
