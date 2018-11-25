d2482c7fe36c0941a00a3df2ffb46b576d3ebb9a
ottoy91 <otto.ylostalo@helsinki.fi>
2018-11-25 21:01:47 +0200

    Exercise4

---
+++ create_human.R
@@ -0,0 +1,2 @@
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
